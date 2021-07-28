8a5269f0e05a21c05937652d4a7ce63db885b22b
Hour-Sirak <hoursirak0102@gmail.com>
2021-07-28 12:34:13 +0700

    a

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 setup(
-	name = 'LCD',         # How you named your package folder (MyLib)
-	packages = ['LCD'],   # Chose the same as "name"
+	name = 'LCDProteus',         # How you named your package folder (MyLib)
+	packages = ['LCDProteus'],   # Chose the same as "name"
 	version = '0.1',      # Start with a small number and increase it with every change you make
 	license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
 	description = 'A library to control lcd on prteus ',   # Give a short description about your library
 	author = 'Group 3',                   # Type in your name
 	author_email = 'grapejam2312@gmail.com',      # Type in your E-Mail
 	url = 'https://github.com/Hour-Sirak/Hour-Sirak',   # Provide either the link to your github or to your website
-	download_url = 'https://github.com/Hour-Sirak/LCD/releases/tag/v_01',    # I explain this later on
+	download_url = 'https://github.com/Hour-Sirak/LCD/archive/refs/tags/v_01.tar.gz',    # I explain this later on
 	keywords = ['lcd', 'proteus'],   # Keywords that define your package best
 	install_requires=[            # I get to this in a second
 			
